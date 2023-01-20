b2ba0f3174aa90e82d694a39a33d16805a820349
Neh Joshi <nehjoshi%5@gmail.com>
2023-01-20 11:24:17 +0530

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+dotenv.config();
+const app = express();
+
+// Connect to MongoDB
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("Connected to DB");
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+//Middleware
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
+
+
+app.listen(5000, () => {
+    connect();
+    console.log("Server started on port 5000");
+})
---
+++ api/routes/hotels.js
@@ -0,0 +1,17 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+const router = express.Router();
+
+//Create
+router.post("/", async (req, res) => {
+
+    const newHotel = new Hotel(req.body);
+
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+})
+export default router;
